95b8ebf4b942aaeeab764acf842c8554e30a4087
Jenna Tauro <jennatauro@gmail.com>
2015-01-05 11:01:35 -0500

    Setup recyclerview

--- app/src/main/java/com/jennatauro/sectionedlistrecyclerview/MainActivity.java
+++ app/src/main/java/com/jennatauro/sectionedlistrecyclerview/MainActivity.java
@@ -2,16 +2,29 @@ package com.jennatauro.sectionedlistrecyclerview;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    RecyclerView mRecyclerView;
+
+    SectionedRecyclerViewAdapter mSectionedRecyclerViewAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mSectionedRecyclerViewAdapter);
     }
 
 }
---
+++ app/src/main/java/com/jennatauro/sectionedlistrecyclerview/SectionedRecyclerViewAdapter.java
@@ -0,0 +1,24 @@
+package com.jennatauro.sectionedlistrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by jennatauro on 2015-01-05.
+ */
+public class SectionedRecyclerViewAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
